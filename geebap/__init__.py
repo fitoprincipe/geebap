5658a23db7996a898f5e568dea013b59422611c5
Rodrigo <rprincipe@ciefap.org.ar>
2017-12-22 08:49:00 -0300

    new version

--- geebap/__init__.py
+++ geebap/__init__.py
@@ -14,7 +14,7 @@ __title__ = "BestAvailablePixel"
 __summary__ = "Generate a 'Best Available Pixel' image in Google Earth Engine"
 __uri__ = ""
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 __author__ = "Rodrigo E. Principe"
 __email__ = "rprincipe@ciefap.org.ar"
