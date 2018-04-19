090bcccbda8fa5de1c99e1affa95a8142ff8ba31
Rodrigo <rprincipe@ciefap.org.ar>
2018-04-19 09:59:04 -0300

    add processing time lapse count to test

--- geebap/tests/test_bap.py
+++ geebap/tests/test_bap.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 import unittest
 import ee
-from .. import satcol, scores, bap, season, masks, filters, functions
+from .. import satcol, scores, bap, season, masks, filters
 from geetools import tools
 from .. import __version__
+import time
 
 ee.Initialize()
 
@@ -32,6 +33,7 @@ class TestBAP(unittest.TestCase):
         self.pindice = scores.Index()
         self.pout = scores.Outliers(("ndvi",))
         self.pdoy = scores.Doy()
+        self.thres = scores.Threshold()
 
         # SITES
         self.sitio = ee.Geometry.Polygon(
@@ -47,7 +49,8 @@ class TestBAP(unittest.TestCase):
                          # colgroup=self.coleccion,
                          season=self.temporada,
                          scores=(self.pindice, self.pmascpor, self.psat,
-                                 self.pout, self.pop, self.pdoy, pmulti),
+                                 self.pout, self.pop, self.pdoy, self.thres,
+                                 pmulti),
                          masks=(self.nubes,),
                          filters=(self.filtro,),
                          )
@@ -55,7 +58,11 @@ class TestBAP(unittest.TestCase):
         # objbap.debug = True
         sitio = self.sitio
 
+        t0 = time.time()
         unpix = objbap.bestpixel(sitio, indices=("ndvi",))
+
+        t1 = time.time()
+
         img = unpix.image
         col = unpix.col
 
@@ -73,5 +80,51 @@ class TestBAP(unittest.TestCase):
         self.assertIsInstance(value, dict)
 
         visual = img.visualize(bands=['NIR','SWIR','RED'], min=0, max=0.5)
-        url = visual.getThumbUrl({'region':tools.getRegion(img)})
+        url = visual.getThumbUrl({'region':tools.getRegion(sitio)})
         print('Result:', url)
+        print('Process time', t1-t0)
+
+        tools.img2asset(img,
+                        'TESTS/BAP/bestcomposite-0-1-5dev',
+                        region=tools.getRegion(sitio))
+
+    def test_fast_composite(self):
+        pmulti = scores.MultiYear(2016, self.temporada)
+        objbap = bap.Bap(year=2016,
+                         # colgroup=self.coleccion,
+                         season=self.temporada,
+                         scores=(self.pindice, self.pmascpor, self.psat,
+                                 self.pout, self.pop, self.pdoy, self.thres,
+                                 pmulti),
+                         masks=(self.nubes,),
+                         filters=(self.filtro,),
+                         )
+
+        # objbap.debug = True
+        sitio = self.sitio
+
+        t0 = time.time()
+        unpix = objbap.fast_composite(sitio, indices=("ndvi",))
+
+        t1 = time.time()
+        img = unpix
+
+        self.assertIsInstance(img, ee.Image)
+
+        idict = img.getInfo()
+        self.assertIsInstance(idict, dict)
+
+        value = tools.get_value(img, self.centroid, 30, 'client')
+        print(value)
+
+        self.assertIsInstance(value, dict)
+
+        visual = img.visualize(bands=['NIR','SWIR','RED'], min=0, max=0.5)
+        url = visual.getThumbUrl({'region':tools.getRegion(sitio)})
+        print('Result:', url)
+        print('Properties', img.getInfo()['properties'])
+        print('Process time', t1-t0)
+
+        tools.img2asset(img,
+                        'TESTS/BAP/fastcomposite-0-1-5dev',
+                        region=tools.getRegion(sitio))
