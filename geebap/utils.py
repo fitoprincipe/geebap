a087011790f697553fc06bcd3904e2b4b386ce32
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2019-05-13 11:29:14 -0300

    improve serialization of objects

--- geebap/utils.py
+++ geebap/utils.py
@@ -7,21 +7,37 @@ def serialize(obj, name=None, result=None):
     if result is None:
         result = {}
 
-    objname = obj.__class__.__name__
-    if name is None:
-        name = objname
+    def make_name(obj, name=None):
+        objname = obj.__class__.__name__
+        if name is None:
+            # name = objname
+            return '({})'.format(objname)
+        return '{} ({})'.format(name, objname)
 
-    name = '{} ({})'.format(name, objname)
+    name = make_name(obj, name)
 
-    attrs = {}
-    result[name] = attrs
-    for k, v in obj.__dict__.items():
-        try:
-            d = v.__dict__
-        except AttributeError:
-            attr_name = '{} ({})'.format(k, v.__class__.__name__)
-            attrs[attr_name] = v
-            continue
+    try:
+        # If it is an object, it has a __dict__
+        obj_attr = obj.__dict__
+    except AttributeError:
+        # If it's NOT an object
+        if isinstance(obj, (tuple, list)):
+            newlist = []
+            for element in obj:
+                newlist.append(serialize(element))
+            result[name] = newlist
+        elif isinstance(obj, (dict,)):
+            newdict = {}
+            for key, element in obj.items():
+                newdict[key] = serialize(element)
+            result[name] = newdict
         else:
-            serialize(v, k, attrs)
+            result[name] = obj
+    else:
+        # If it IS an object
+        attrs = {}
+        result[name] = attrs
+        for attr_name, attr_value in obj_attr.items():
+            serialize(attr_value, attr_name, attrs)
+
     return result
